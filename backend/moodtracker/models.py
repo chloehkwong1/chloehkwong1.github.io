f20c9bd4346b8694ac55c6f2cec3d72af2b11efb
Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
2022-07-07 15:08:18 +0100

    Adding an mood entry model
    Co-Authored-By: Onyela Ogah <83657534+onyela-o@users.noreply.github.com>
    Co-Authored-By: Teemerte Teccola <94635933+Teccolat@users.noreply.github.com>

--- backend/moodtracker/models.py
+++ backend/moodtracker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class MoodEntry(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    mood = models.CharField(max_length=120)
+    mood_influences = models.TextField()
+
+    def _str_(self):
+        return 'My mood at {self.time} on {self.date}'
+
+# class MoodTypes(models.Model):
