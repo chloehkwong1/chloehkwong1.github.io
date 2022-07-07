d290d589bdbb2e18b4f7913d3e264f63baa3c10e
Teemerte Teccola <teemerte.teccola@digital.trade.gov.uk>
2022-07-07 16:12:45 +0100

    intitial migration of mood entry

--- backend/moodtracker/admin.py
+++ backend/moodtracker/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import MoodEntry
+
+class MoodEntryAdmin(admin.ModelAdmin):
+    list_display = ('date', 'time', 'mood','mood_influences')
 
 # Register your models here.
+
+admin.site.register(MoodEntry,  MoodEntryAdmin)
+  
+
