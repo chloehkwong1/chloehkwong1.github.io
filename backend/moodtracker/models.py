b01c4ad73b10df6e7f5346a09f8e149ff6f17ad3
Onyela Ogah <83657534+onyela-o@users.noreply.github.com>
2022-07-08 10:44:53 +0100

    Update mood entries fields

--- backend/moodtracker/models.py
+++ backend/moodtracker/models.py
@@ -1,13 +1,28 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
 class MoodEntry(models.Model):
+
+    MOOD_CHOICES = [
+        ('CALM', 'Calm'),
+        ('EXCITED', 'Excited'),
+        ('ANGRY', 'Angry'),
+        ('SAD', 'Sad'),
+        ('FRUSTRATED', 'Frustrated'),
+        ('SCARED', 'Scared'),
+        ('RESTED', 'Rested'),
+        ('HAPPY', 'Happy'),
+        ('TIRED', 'Tired'),
+    ]
+
+    HELP_TEXT = 'How are you feeling today?'
+
     date = models.DateField()
     time = models.TimeField()
-    mood = models.CharField(max_length=120)
-    mood_influences = models.TextField()
+    mood = models.CharField(max_length=10, choices=MOOD_CHOICES, help_text=HELP_TEXT)
+    mood_influences = models.TextField(blank=True)
 
     def _str_(self):
         return 'My mood was {self.mood} at {self.time} on {self.date}'
 
-# class MoodTypes(models.Model):
+
