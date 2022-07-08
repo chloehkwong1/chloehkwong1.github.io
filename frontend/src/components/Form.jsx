5118139bc4da0a10ca5b7987c857273802124a06
Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
2022-07-08 16:56:49 +0100

    WIP form
    Co-Authored-By: Teemerte Teccola <94635933+Teccolat@users.noreply.github.com>
    Co-Authored-By: Onyela Ogah <83657534+onyela-o@users.noreply.github.com>

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
-const Form = () => <h1>This is the form</h1>;
+const Form = () => {
+  return (
+    <form>
+      <input id="tired-mood" type="radio" value="Tired" />
+      <label for="tired-mood">Tired</label>
+      <input type="radio" value="Happy" />
+      <input type="radio" value="Rested" />
+      <input type="radio" value="Excited" />
+      <input type="radio" value="Sad" />
+      <input type="radio" value="Angry" />
+      <input type="radio" value="Calm" />
+      <input type="radio" value="Frustrated" />
+      <input type="radio" value="Scared" />
+    </form>
+  );
+};
 
 export default Form;
