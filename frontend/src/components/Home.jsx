8035a5caf1b148a7aeaad9c9d997f6c30bf7c082
Onyela Ogah <83657534+onyela-o@users.noreply.github.com>
2022-07-09 16:34:31 +0100

    Update copy and style date and time on home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -9,15 +9,38 @@ function Home() {
       .then((data) => setMoodEntry(data));
   }, []);
 
+  const formatDate = (entryDate) => {
+    var dateToFormat = new Date(entryDate)
+    var options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    var formattedDate = dateToFormat.toLocaleDateString("default", options);
+    return formattedDate;
+  }
+
+  const formatTime = (entryTime) => {
+    const hourOnly = entryTime.slice(0, 2)
+    if (hourOnly >= 12)
+      return `${hourOnly-12}pm`
+    return `${hourOnly[1]}am`;
+  }
+
+
   const renderMoodEntries = () =>
     moodEntry.map((item, index) => (
       <ul key={`mood-entry-list-item-${index}`}>
         <div>
           <h1>
-            Mood at {item.date} {item.time}
+            On {formatDate(item.date)} at {formatTime(item.time)}, you felt:
+            <div>{item.mood}</div>
           </h1>
-          <h1>Mood is: {item.mood}</h1>
-          <span>{item.mood_influences}</span>
+          <span>{item.mood_influences && <div>The reason you gave was: { item.mood_influences}</div>} </span>
+          <br />
+          <br />
+          <br />
         </div>
       </ul>
     ));
