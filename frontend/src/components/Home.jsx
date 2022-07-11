2915a51e8b75db7b0ff6f33b6037dbd1ae790802
Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
2022-07-11 12:53:50 +0100

    Removed line breaks

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
   }, []);
 
   const formatDate = (entryDate) => {
-    var dateToFormat = new Date(entryDate)
+    var dateToFormat = new Date(entryDate);
     var options = {
       weekday: "long",
       year: "numeric",
@@ -19,29 +19,32 @@ function Home() {
     };
     var formattedDate = dateToFormat.toLocaleDateString("default", options);
     return formattedDate;
-  }
+  };
 
   const formatTime = (entryDate, entryTime) => {
-    const timeToFormat = new Date(entryDate + 'T' + entryTime);
-    const formattedTime = timeToFormat.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-    if (formattedTime[0] === '0')
-      return formattedTime.slice(1);
-    return formattedTime
-  }
-
+    const timeToFormat = new Date(entryDate + "T" + entryTime);
+    const formattedTime = timeToFormat.toLocaleTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+    if (formattedTime[0] === "0") return formattedTime.slice(1);
+    return formattedTime;
+  };
 
   const renderMoodEntries = () =>
     moodEntry.map((item, index) => (
       <ul key={`mood-entry-list-item-${index}`}>
         <div className="note">
           <h1>
-            On {formatDate(item.date)} at {formatTime(item.date, item.time)}, you felt:
+            On {formatDate(item.date)} at {formatTime(item.date, item.time)},
+            you felt:
             <div>{item.mood}</div>
           </h1>
-          <span>{item.mood_influences && <div>The reason you gave was: { item.mood_influences}</div>} </span>
-          <br />
-          <br />
-          <br />
+          <span>
+            {item.mood_influences && (
+              <div>The reason you gave was: {item.mood_influences}</div>
+            )}{" "}
+          </span>
         </div>
       </ul>
     ));
